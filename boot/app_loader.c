b30e8ef965a825e7a12aec8d923e068be62b40d7
mlokcewicz <mlokcewicz@gmail.com>
2026-08-25 01:25:58 +0200

    Implement app_loader logic

--- boot/app_loader.c
+++ boot/app_loader.c
@@ -8,6 +8,7 @@
 #include "app_loader.h"
 
 #include <stdio.h>
+#include <string.h>
 
 #include <stm32l4xx.h>
 
@@ -19,6 +20,11 @@
 
 #define DEFAULT_BOOT_VERSION "v1.2.3"
 
+#define DFU_TIMEOUT_MS 300000 /* 5 minutes */
+#define DFU_BUFFER_SIZE (8 * 1024)
+
+_Static_assert (!(DFU_BUFFER_SIZE % sizeof(uint64_t)), "DFU buffer must be multiplicity of DWORD");
+
 //------------------------------------------------------------------------------
 
 extern const uint8_t __app_start__;
@@ -27,9 +33,29 @@ extern const uint8_t __app_size__;
 static uintptr_t app_start = (uintptr_t)&__app_start__;
 static uintptr_t app_size = (uintptr_t)&__app_size__;
 
+__aligned(sizeof(uint64_t)) static uint8_t app_loader_buf[DFU_BUFFER_SIZE];
+
+static volatile uint32_t buf_tail;
+static volatile uint32_t buf_head;
+
+static uint32_t last_rx_tick;
+
 //------------------------------------------------------------------------------
 
 typedef void(*reset_handler_t)(void);
+
+//------------------------------------------------------------------------------
+
+static bool get_dfu_timeout_elapsed(void)
+{
+    if ((HAL_GetTick() - last_rx_tick) > DFU_TIMEOUT_MS)
+    {
+        printf("DFU receive timeout\n");
+        return true;
+    }
+
+    return false;
+}
 
 //------------------------------------------------------------------------------
 
@@ -55,9 +81,86 @@ bool app_loader_is_dfu_requested(void)
     return requested;
 }
 
+void uart_rx_idle_cb(uint16_t size)
+{
+    buf_head = size;
+    buf_head %= DFU_BUFFER_SIZE;
+
+    last_rx_tick = HAL_GetTick();
+}
+
+void uart_rx_err_cb(void)
+{
+
+}
+
+void flash_dword_program_cb(void)
+{
+    iwdg_feed();
+    led_toggle();
+}
+
 bool app_loader_perform_dfu(void)
 {
-    /* TODO: Implement UART data reception and FLASH write */
+
+    if (received_app_size == 0 || received_app_size > app_size)
+    {
+        printf("Invalid application size\n");
+        return false;
+    }
+
+    buf_head = 0;
+    buf_tail = 0;
+    last_rx_tick = HAL_GetTick();
+
+    uart_start_rx(app_loader_buf, sizeof(app_loader_buf));
+
+
+    iwdg_feed();
+
+    if (!flash_erase(app_start, received_app_size))
+    {
+        printf("Flash erase error\n");
+        return false;
+    }
+
+    uint32_t curr_flash_address = app_start;
+    uint32_t end_address = app_start + received_app_size;
+    uint64_t last_word_buf = 0xFFFFFFFFFFFFFFFF;
+
+    while (curr_flash_address < end_address)
+    {
+        uint32_t temp_head = (buf_head >= buf_tail) ? buf_head : DFU_BUFFER_SIZE; // Prevent buffer flip
+        uint32_t bytes_to_write = temp_head - buf_tail;
+        uint32_t dwords_to_write = bytes_to_write / sizeof(uint64_t);
+        uint32_t bytes_left = end_address - curr_flash_address;
+        uint64_t *write_ptr = (uint64_t*)&app_loader_buf[buf_tail];
+        
+        /* Handle last non-full word */
+        if (bytes_left < sizeof(uint64_t) && bytes_left <= bytes_to_write)
+        {
+            dwords_to_write = 1;
+            memcpy(&last_word_buf, app_loader_buf + buf_tail, bytes_left);
+            write_ptr = &last_word_buf;
+        }
+
+        if (dwords_to_write)
+        {
+            if (!flash_program(curr_flash_address, write_ptr, dwords_to_write))
+            {
+                printf("Flash program error\n");
+                return false;
+            }
+
+            curr_flash_address += dwords_to_write * sizeof(uint64_t);
+            buf_tail = (buf_tail + dwords_to_write * sizeof(uint64_t)) % DFU_BUFFER_SIZE;
+        }
+
+        iwdg_feed();
+
+        if (get_dfu_timeout_elapsed())
+            return false;
+    }
 
     iwdg_feed();
 
